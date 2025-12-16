d82bd6bd56482c65e73f316ac391ba5a58538683
Alexis Boulic <boulicalexis@gmail.com>
2025-12-16 16:17:12 +0100

    Feat: Deploy-Infra Action

--- main.tf
+++ main.tf
@@ -16,3 +16,36 @@ terraform {
     encrypt = true
   }
 }
+
+# 1. S3 Bucket for Data and ML Models
+resource "aws_s3_bucket" "data_bucket" {
+  bucket = "s3-g1mg06"  
+  force_destroy = true 
+}
+
+# Enable versioning for the data bucket (Best Practice)
+resource "aws_s3_bucket_versioning" "data_bucket_versioning" {
+  bucket = aws_s3_bucket.data_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# 2. ECR Repository for Docker Images
+resource "aws_ecr_repository" "mlops_repo" {
+  name                 = "ecr-g1mg06" 
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+# Output the names to confirm creation
+output "s3_bucket_name" {
+  value = aws_s3_bucket.data_bucket.bucket
+}
+
+output "ecr_repository_url" {
+  value = aws_ecr_repository.mlops_repo.repository_url
+}
